d7d68d1d8537b7c3e80308b5da8b2284f5edaf65
tTanya98 <chichkinat98@gmail.com>
2023-03-17 20:30:43 +0200

    Update tsk-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startB = document.querySelector('[data-start]');
+const stopB = document.querySelector('[data-stop]');
 
+let intervalid;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+function changeBackgroundColor() {
+    document.body.style.backgroundColor = getRandomHexColor();
+}
+
+startB.addEventListener('click', () => {
+    if (intervalid)
+    return;
+    intervalid = setInterval(changeBackgroundColor, 1000);
+});
+
+stopB.addEventListener('click', () => {
+    clearInterval(intervalid);
+});
