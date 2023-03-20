a5ce355d09198ef61b1059f72d215739d806d1a4
tTanya98 <chichkinat98@gmail.com>
2023-03-20 18:55:05 +0200

    update tsk-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,22 +1,29 @@
 const startB = document.querySelector('[data-start]');
 const stopB = document.querySelector('[data-stop]');
+const bodyF = document.querySelector('body');
 
-let intervalid;
+let timeId = null;
 
-function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
-}
+startB.addEventListener('click', onStart);
+stopB.addEventListener('click', onStop);
+stopB.disabled = true;
 
 function changeBackgroundColor() {
     document.body.style.backgroundColor = getRandomHexColor();
 }
 
-startB.addEventListener('click', () => {
-    if (intervalid)
-    return;
-    intervalid = setInterval(changeBackgroundColor, 1000);
-});
+function onStart() {
+    timerId = setInterval(changeBackgroundColor, 1000);
+    startB.setAttribute('disabled', true);
+    stopB.removeAttribute('disabled');
+}
 
-stopB.addEventListener('click', () => {
-    clearInterval(intervalid);
-});
+function onStop() {
+    clearInterval(timerId);
+    startB.removeAttribute('disabled');
+    stopB.setAttribute('disabled', true);
+}
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
